8f96ed5e734254cfbeeb2e1416365a72a9bba370
etmihailo <mihailo@entretechno.com>
2025-07-08 20:25:35 -0400

    Test basic form component

--- app/controllers/github_events_controller.rb
+++ app/controllers/github_events_controller.rb
@@ -3,7 +3,21 @@ class GithubEventsController < ApplicationController
 
   def index; end
 
-  def create; end
+  def create
+    redirect_to github_event_path(params[:username])
+  end
 
-  def show; end
+  def show
+    @username = params[:username]
+    Rails.logger.info "@@@@@@@@@@@@@@@@@#{@username}"
+    events = Array.new(50) do |i|
+      {
+        id: i + 1,
+        name: "#{@username}#{i + 1}"
+      }
+    end
+    Rails.logger.info "!!!!!!!!!!!!!!!!!!!#{events}"
+
+    @pagy, @paginated_events = pagy_array(events, items: 10)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   root "github_events#index"
-  resources :github_events, only: [ :index, :create, :show ]
+  resources :github_events, only: [ :index, :create, :show ], param: :username
   post "/graphql", to: "graphql#execute"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
