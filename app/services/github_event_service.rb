bd177da8c3ee4911e474af12523731d51be06a38
etmihailo <mihailo@entretechno.com>
2025-07-08 20:39:34 -0400

    Parse/format reposne properly

--- app/services/github_event_service.rb
+++ app/services/github_event_service.rb
@@ -6,9 +6,29 @@ class GithubEventService
     Rails.logger.info "$$$$$$$$$$$$$$#{response}"
 
     if response.success?
-      response.body
+      parse_events(JSON.parse(response.body))
     else
       []
     end
+  end
+
+  def self.parse_events(events)
+    events.map do |event|
+      case event["type"]
+      when "CreateEvent"
+        { type: "Repo Created", repo: event["repo"]["name"], time: event["created_at"] }
+      when "PullRequestEvent"
+        pr = event.dig("payload", "pull_request")
+        next unless pr
+        merged = pr["merged_at"].present?
+        {
+          type: merged ? "PR Merged" : "PR Opened",
+          repo: event["repo"]["name"],
+          time: event["created_at"]
+        }
+      when "PushEvent"
+        { type: "Pushed Commit", repo: event["repo"]["name"], time: event["created_at"] }
+      end
+    end.compact
   end
 end
